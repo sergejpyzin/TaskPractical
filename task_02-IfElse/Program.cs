3805b1e8060776faf2e1b7ea638260dfe1993291
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-11 11:20:54 +0300

    created task_02-IfElse - day of the week

---
+++ task_02-IfElse/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+// Запрашиваем информацию у пользователя
+Console.Write("Введите номер дня недели: ");
+int request = Convert.ToInt32(Console.ReadLine());
+
+// Проверяем условие введенной пользователем информации
+if (request == 1) 
+{
+    Console.WriteLine("Понедельник");
+}
+else if (request == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (request == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (request == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (request == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (request == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (request == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+Console.WriteLine("Внимание! Такого дня недели не существует!");
+}
