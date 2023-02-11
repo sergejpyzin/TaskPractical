5969137ca4ab9c65f1f30ecf2285442af35f3a6b
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-11 10:11:40 +0300

    created task_02 - day of the week switch-case

---
+++ task_02/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.Write("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+        System.Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        System.Console.WriteLine("Вторник");
+        break;
+    case 3:
+        System.Console.WriteLine("Среда");
+        break;
+    case 4:
+        System.Console.WriteLine("Четверг");
+        break;  
+    case 5:
+        System.Console.WriteLine("Пятница");
+        break;
+    case 6:
+        System.Console.WriteLine("Суббота");
+        break;
+    case 7:
+        System.Console.WriteLine("Воскресенье");
+        break;      
+    default:
+        System.Console.WriteLine("Внимание! Такого дня недели не существует!");
+        break;
+}
