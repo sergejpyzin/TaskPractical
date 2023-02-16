52980080ccf75d63726ef9a6b4410433722aa4d8
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-16 08:32:57 +0300

    Created SeminareTwo_task11... folder

---
+++ SeminareTwo_task11 - MidleNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 11: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+ */
+
+// Задаем случайное трехзначное число
+int number = Random.Shared.Next(100, 1000);
+System.Console.WriteLine(number);
+
+// Выполняем условия задачи
+int lastDigit = number % 10;
+int middleDigit = (number / 10) % 10;
+int firstDigit = (number / 100) % 10;
+// Console.WriteLine($"{firstDigit} {middleDigit} {lastDigit}");
+
+int resNumber = firstDigit * 10 + lastDigit;
+
+// Выводим результат
+System.Console.WriteLine(resNumber);
+
+
+
