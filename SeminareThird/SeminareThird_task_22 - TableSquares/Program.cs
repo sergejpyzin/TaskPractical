a5024ba066bfccae1baa835a793933d628f60f99
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-18 10:34:18 +0300

    Created SeminareThird_task_22...

---
+++ SeminareThird/SeminareThird_task_22 - TableSquares/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+ */
+
+// Создаем метод запроса информации у пользователя - DataRequest
+
+int DataRequest(string userMassedge)
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write(userMassedge);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        catch (System.Exception)
+        {
+
+            Console.WriteLine($"Ошибка! Попробуйте еще раз");
+            continue;
+
+        }
+    }
+}
+
+// Запрашиваем информацию у пользователя
+int N = DataRequest("Введите число: ");
+
+Console.WriteLine($"Число\t|  Квадрат");
+
+
+//  Проводим вычисления
+for (int i = 0; i <= N; i++)
+{
+    double result = Math.Pow(i, 2);
+    Console.WriteLine($"{i}\t|\t{result}");
+}
